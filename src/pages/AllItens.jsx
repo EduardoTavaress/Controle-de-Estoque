7134d8ec2b04a59cb6a6ef8e0823dfb739eb762c
Eduardo Tavares <eduardotavares291@gmail.com>
2025-11-26 13:01:27 -0300

    Pag de exibição de itens atualizada

--- src/pages/AllItens.jsx
+++ src/pages/AllItens.jsx
@@ -1,8 +1,22 @@
 export default function AllItems() {
     return (
         <div>
-            <h2>Todos os Itens no DB</h2>
-            {/* Lógica para exibir todos os itens */}
+            <h3>Nome do produto do BD</h3>
+            <button>Atualizar</button>
+            <button>Excluir</button>
+
+            <div>
+                <span>Categoria: do Bd</span>
+                <span>Quatidade em estoque: do BD</span>
+                <span>Preço: do BD</span>
+            </div>
+            <p>descrição do BD</p>
+
+           
+
+            <p>cadastro: do BD - <small>Atualizado em: do BD</small></p> 
+            
+
         </div>
     );
 }
