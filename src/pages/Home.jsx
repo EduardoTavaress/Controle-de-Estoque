2583e4d57c83f168c17e5a345b09864f2997f24b
Eduardo Tavares <eduardotavares291@gmail.com>
2025-11-26 14:40:46 -0300

    funcionalidade na opção VER e funções nos Card foram adcionados a Pag Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import data from "../database.json";
 
 export default function Home() {
-  return (
+  
+  const navigate = useNavigate();
 
-    
+  return (
 
     <div>
 
@@ -12,39 +15,76 @@ export default function Home() {
       <h1>Dashboard</h1>
 
       <section>
+        {data.map(item => (
+          <div key={item.id}>
+           
         <div>
             <p>Diversidade de itens</p>
-            <span>qtd do BD</span>
+            <span>{data.length}</span>
         </div>
         <div>
             <p>Inventário total</p>
-            <span>qtd do BD</span>
+            <span>{data.reduce((total, item) => total + item.quantity, 0)}</span>
         </div>
         <div>
             <p>Itens recentes</p>
-            <span>qtd do BD</span>
+            <span>{data.filter(item => {
+              const days = 7; // Defina o número de dias para considerar como "recente"
+              const hoje = new Date();
+              const dataItem = new Date(item.createdAt);
+              const diff = (hoje - dataItem) / (1000 * 60 * 60 * 24);
+              return diff <= days;
+            }).length}</span>
         </div>
         <div>
             <p>Itens acabando</p>
-            <span>qtd do BD</span>
+            <span>{data.filter(item => item.quantity < 6).length}</span>
         </div>
-        <div>
-            <p>Itens Recentes</p>
-            <span>itens do BD</span>
 
-            <p>Ações</p>
-            <button>Ver</button>
-        </div>
         <div>
+          {data.filter(item => {
+            const days = 7;
+            const hoje = new Date();
+            const dataItem = new Date(item.createdAt);
+            const diff = (hoje - dataItem) / (1000 * 60 * 60 * 24);
+            return diff <= days;
+          }).map(item => (
+            <div key={item.id}>
+
+              <p>Itens Recentes</p>
+              <span>{item.name}</span>
+
+              <p>Ações</p>
+
+              <button onClick={() => navigate(`/stock-item/ver/${item.id}`)}>
+                  Ver
+              </button>
+
+            </div>
+          ))}
+        </div>
+
+        <div>
+          {data.filter(item => item.quantity < 6).map(item => (
+            <div key={item.id}>
             <p>Itens Acabando</p>
-            <span>itens do BD</span>
+            <span>{item.name}</span>
 
             <p>Qtd.</p>
-            <span>qtd do BD</span>
+            <span>{item.quantity}</span>
 
             <p>Ações</p>
-            <button>Ver</button>
+
+            <button onClick={() => navigate(`/stock-item/ver/${item.id}`)}>
+                  Ver
+            </button>
+
+            </div>
+          ))}
         </div>
+
+          </div>
+        ))}
       </section>
     </div>
   );
