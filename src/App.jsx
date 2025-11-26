67c016140703634eda0523ef2ccbd345401831a1
Eduardo Tavares <eduardotavares291@gmail.com>
2025-11-26 15:06:03 -0300

    funcionalidade na opção VER e funções nos Card foram adcionados a Pag Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import router from "./router";
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <RouterProvider router={router} />
     </div>
   );
--- src/pages/NewItem.jsx
+++ src/pages/NewItem.jsx
@@ -1,4 +1,5 @@
 export default function NewItem() {
+    
     return (
         <div>
             <label htmlFor="name">Nome</label>
