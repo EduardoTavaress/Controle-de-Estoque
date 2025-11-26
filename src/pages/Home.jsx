47ec20b11136b1b19870fd35fb43b3f8d3601364
Eduardo Tavares <eduardotavares291@gmail.com>
2025-11-26 10:37:24 -0300

    Criação da página Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+
+    
+
+    <div>
+
+    <Header />
+
+      <h1>Dashboard</h1>
+
+      <section>
+        <div>
+            <p>Diversidade de itens</p>
+            <span>qtd do BD</span>
+        </div>
+        <div>
+            <p>Inventário total</p>
+            <span>qtd do BD</span>
+        </div>
+        <div>
+            <p>Itens recentes</p>
+            <span>qtd do BD</span>
+        </div>
+        <div>
+            <p>Itens acabando</p>
+            <span>qtd do BD</span>
+        </div>
+        <div>
+            <p>Itens Recentes</p>
+            <span>itens do BD</span>
+
+            <p>Ações</p>
+            <button>Ver</button>
+        </div>
+        <div>
+            <p>Itens Acabando</p>
+            <span>itens do BD</span>
+
+            <p>Qtd.</p>
+            <span>qtd do BD</span>
+
+            <p>Ações</p>
+            <button>Ver</button>
+        </div>
+      </section>
+    </div>
+  );
+}
