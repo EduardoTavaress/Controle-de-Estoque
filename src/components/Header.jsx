92fbb5ae70392bf767803688b5d6d7c113bf2cac
Eduardo Tavares <eduardotavares291@gmail.com>
2025-11-26 10:46:59 -0300

    Criação do component Header, com a Nav do inicio da pag

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function Header () {
+    return (
+        <header>
+
+            <h2>REACT STOCK</h2>
+
+            <nav>
+                <Link to="/">Início</Link>
+                <Link to="/items">Itens</Link>
+            </nav>
+            
+        </header>
+    );
+}
