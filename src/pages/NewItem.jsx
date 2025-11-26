675445c8950bce21c6d28644c39c664bb1ef3d5e
Eduardo Tavares <eduardotavares291@gmail.com>
2025-11-26 13:07:02 -0300

    Atualização do HTML da pag de Add Novo Item

--- src/pages/NewItem.jsx
+++ src/pages/NewItem.jsx
@@ -1,8 +1,22 @@
 export default function NewItem() {
     return (
         <div>
-            <h2>Criar Novo Item</h2>
-            {/* Lógica para criar um novo item */}
+            <label htmlFor="name">Nome</label>
+            <input type="text" id="name" name="name" />
+
+            <label htmlFor="quantity">Quantidade em estoque</label>
+            <input type="number" id="quantity" name="quantity" />
+
+            <label htmlFor="price">Preço</label>
+            <input type="number" id="price" name="price" step="0.01" />
+
+            <label htmlFor="category">Categoria</label>
+            <input type="text" id="category" name="category" /> 
+
+            <label htmlFor="description">Descrição</label>
+            <textarea id="description" name="description"></textarea>
+
+            <button type="submit">Salvar</button>
         </div>
     );
 }
